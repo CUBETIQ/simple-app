8732b3fca3f182c599053798618b5375fae91c8c
Sambo Chea <sombochea@cubetiqs.com>
2021-02-20 12:07:26 +0700

    Initial Commit

---
+++ simpleapp/simpleappApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleappApp.swift
+//  simpleapp
+//
+//  Created by Sambo Chea on 20/2/21.
+//
+
+import SwiftUI
+
+@main
+struct simpleappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
